0e51e7de5743fe7a5f542fd46b6dbd0177d65f87
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-04-29 13:38:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,10 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-      bash_hash = {:railroads => {},
+      bash_hash = {
+        :railroads => {
+          
+        },
 }
         {:pieces =>{number:"4"}},
 }
